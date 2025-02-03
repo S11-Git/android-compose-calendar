b2eaf4d18bbaf81e022a8b368fa51c73a929719a
MELIORA <meliora@s11.dev>
2025-02-03 15:58:52 +0100

    Refactor getCurrentWeekNumber function to improve locale handling and readability

--- app/src/main/java/com/danielrampelt/schedule/MainActivity.kt
+++ app/src/main/java/com/danielrampelt/schedule/MainActivity.kt
@@ -310,11 +310,11 @@ fun BasicDayHeaderPreview(
 }
 
 
-fun getCurrentWeekNumber(
-    date: LocalDate,
-): Int {
-    val weekFields = WeekFields.of(Locale.getDefault())
-    return date.get(weekFields.weekOfWeekBasedYear())
+fun getCurrentWeekNumber(date: LocalDate): Int {
+    val systemLocale = Locale.getDefault(Locale.Category.FORMAT)
+    val weekFields = WeekFields.of(systemLocale)
+    val weekNumber = date.get(weekFields.weekOfWeekBasedYear())
+    return weekNumber
 }
 
 
